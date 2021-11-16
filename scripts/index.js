d0efc989bee0440787405a156e3e2ab2037f1ba2
Timothy <t.wojtylak@gmail.com>
2021-11-15 19:24:33 -0500

    - Added some functions to make request for data - Minor CSS adjustments

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,46 @@
-const elem = document.getElementsByClassName('search-form');
-console.log();
+const apiKey = 'c053e06f';
+const endpoint = 'http://www.omdbapi.com?';
+
+const movies = [
+    'Star Wars',
+    'Harry Potter',
+    'Taken',
+    'Cars'
+];
+
+// Initialization logic
+init();
 
 document.forms[0].onsubmit = (event) => {
     event.preventDefault();
     const searchInput = document.getElementById('search-input');
-    console.log(searchInput.value);
+    const obj = {
+        apiKey: apiKey,
+        t: encodeURI(searchInput.value)
+    };
+    makeRequest(obj);
+}
+
+function makeRequest(searchObj) {
+    const searchString = buildQueryParams(searchObj);
+
+    fetch(endpoint + searchString)
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
+
+function buildQueryParams(obj) {
+    return Object.keys(obj).map(key => {
+        return `${key}=${obj[key]}`;
+    }).join('&');
+}
+
+function init() {
+    movies.forEach(movie => {
+        const obj = {
+            apiKey: apiKey,
+            t: encodeURI(movie)
+        };
+        makeRequest(obj);
+    });
 }
